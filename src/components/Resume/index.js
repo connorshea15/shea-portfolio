3f9dd1f8210680940819a5d47c5613da5760a11b
connorshea15 <connorshea15@gmail.com>
2021-02-08 16:18:10 -0700

    Basically spent all day trying to get footer to be at bottom of resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Resume() {
 
     return (
-        <div className="text-center">
+        <div className="text-center resume-page">
             <h3 className="text-center">Download My <a href="https://docs.google.com/document/d/1pkfHg0PUpo0HLfnpJjwBrvLLf8RLRVnBbrfHMrw41Dk/export?format=pdf" target="_blank" rel="noopener noreferrer">Resume</a></h3>
             <div className="flex-row flex-center px-2">
                 <div className="mx-2">
